b400730f36ee8c89293cf341fdb38b19d0e97cdf
ZeroTwo-M1000 <BioArtex@yandex.ru>
2024-10-24 04:41:30 +0300

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export const purge = ["./src/**/*.{js,vue}", "./public/index.html"]
+export const darkMode = false
+export const theme = {
+    extend: {},
+}
+export const variants = {
+    extend: {},
+}
+export const plugins = []
